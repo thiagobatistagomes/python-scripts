24f65d3af5aa4f513e52aff335fdb3b6bf9fd584
thiagobatistagomes <122994560+thiagobatistagomes@users.noreply.github.com>
2024-01-10 17:25:51 -0300

    Primeiros Desafios
    Primeiros passos em Python

---
+++ exercicios-modulo1/Desafios/Desafio03.py
@@ -0,0 +1,5 @@
+print('====== DESAFIO 03 ======')
+n1 = input('Primeiro numero: ')
+n2 = input('Segundo numero: ')
+soma = int(n1) + int(n2)
+print('Soma:',soma)
---
+++ exercicios-modulo1/teste01.py
@@ -0,0 +1,5 @@
+nome = input('Qual é o seu nome?')
+idade = input('Qual é a sua idade?')
+altura = input('Qual é a sua altura?')
+peso = input('Qual é o seu peso?')
+print(nome, idade, altura, peso)
