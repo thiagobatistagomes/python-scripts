39d5891ae4310c5d8ad98900dc1d479ebfe0e4c0
thiagobatistagomes <122994560+thiagobatistagomes@users.noreply.github.com>
2024-01-15 14:10:08 -0300

    ex aula 08

--- exercicios-modulo1/Desafios/Desafio015.py
+++ exercicios-modulo1/Desafios/Desafio015.py
@@ -1,5 +1,5 @@
 #  Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
-print('====== DESADIO 15 ======')
+print('====== DESAFIO 15 ======')
 dias = int(input('Quantos dias alugados? '))
 km = int(input('Quantos Km rodados? '))
 preco = (60 * dias) + (0.15 * km)
